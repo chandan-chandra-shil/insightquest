de1fc01f5dbb283c6182c668ded64daf03509857
soutammolick829 <soutammollick829@gmail.com>
2023-08-11 00:30:47 +0600

    created a navbar

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,10 +1,9 @@
 
-
 const Home = () => {
   return (
-    <div className="text-center">
-      <h1 className="text-3xl font-bold underline">Welcome </h1>
-      <button className="btn btn-success">Our all team member</button>
+    <div>
+      <h2>This is home page</h2>
+
     </div>
   );
 };
