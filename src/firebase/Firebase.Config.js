9b6839ea8ebfc75cf1604a76efe9333c6a385619
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-08-11 02:13:36 +0600

    add firebase

---
+++ src/firebase/Firebase.Config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgg0eXQq-NV5HS555QUljYL9ToGOJwS7w",
+  authDomain: "insightquest-33c00.firebaseapp.com",
+  projectId: "insightquest-33c00",
+  storageBucket: "insightquest-33c00.appspot.com",
+  messagingSenderId: "507650595947",
+  appId: "1:507650595947:web:223d5d222b550f0461e52e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
