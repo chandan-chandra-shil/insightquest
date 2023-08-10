e05792c1ea5abcd26a3ed259b0f0595caf40a382
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-08-10 17:52:33 +0600

    basic project setup

---
+++ src/pages/home/home/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold underline">Welcome </h1>
+      <button className="btn btn-success">Our all team member</button>
+    </div>
+  );
+};
+
+export default Home;
