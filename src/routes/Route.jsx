19b6af17011d56ad3746dff3d8fe10c6cb9b3587
mehedi001 <mh.hridoy5070@gmail.com>
2023-08-11 17:19:35 +0600

    fix some file

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter,  } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/home/home/Home";
 import ErrorPage from "../pages/errorPage/ErrorPage";
+import SurveyCard from "../pages/home/surveyCard/SurveyCard";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element:<Home/>
+      },
+      {
+        path:"services",
+        element:<SurveyCard></SurveyCard>
       }
     ]
   },
